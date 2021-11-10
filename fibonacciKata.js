9ff88e5132f988d749f85e1d5c37844d1ddc0950
MikeTheRooster <miketherooster63@gmail.com>
2021-11-10 03:43:09 -0600

    Refactoring

--- fibonacciKata.js
+++ fibonacciKata.js
@@ -1,55 +1,22 @@
-//TODO: Refactor
+//BUD refactor
 
 let fibbFunct = (n) => {
-    let sum;
-    if (n < 1) {
-        // console.log("here");
-            sum = [];
-            console.log(sum);
-         console.log(`There are no numbers in the sequence.`);
+  let sum = [0 , 1];
+  if (n < 1) {
+    sum = [];
+    console.log(`There are no numbers in the sequence.`);
+  } else {  
+    for (i = 2; i < n; i++) {
+        let sumLen = sum.length;
+      sum.push(sum[sumLen - 2] + sum[sumLen - 1]);
     }
-    else if (n === 1) {
-        // console.log("here");
-            sum = [0];
-         console.log(sum);
-    }
-    /**  else if (n === 2) {
-    *    console.log("here2");
-    *     sum = [0, 1];
-    *    console.log(sum);
-    *   }
-    */
-    else {
-        //sum last two values
-        // console.log("here3");
-        let prev2Num = 0;
-        let prevNum = 1;
-             sum = [prev2Num, prevNum];
-        console.log(sum);
-        /**Shift previous numbers to variables prev2Num and prevNum
-         * sum.length - 1 = prev2Num 
-         * sum.length = prevNum
-         * */
-        for (i = 2; sum.length < n; i++) {
-            sum.push(( prev2Num + prevNum));
-            console.log(`this is sum after push: ${sum}`);
-            prev2Num = sum[(i -1)];
-                console.log(`this is value at prev2num: ${prev2Num}`);
-            prevNum = sum[i];
-                console.log(`this is value at prevnum: ${prevNum}`);
-            console.log(sum);    
-        }
-        // return sum
-    }
+  }
+  console.log(sum);
+  return sum;
+};
 
-    return sum;
-   
-}
-
-fibbFunct(4);
-console.log(`\n`);
-fibbFunct(7);
-console.log(`\n`); 
 fibbFunct(0);
 console.log(`\n`);
 fibbFunct(2);
+console.log(`\n`);
+fibbFunct(10);
