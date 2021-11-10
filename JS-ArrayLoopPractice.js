1068d5114cfc40566ae541272ee669d689410f9b
MikeTheRooster <miketherooster63@gmail.com>
2021-11-10 11:59:58 -0600

    JS Dom manip.

---
+++ JS-ArrayLoopPractice.js
@@ -0,0 +1,56 @@
+/** Nested for loop
+ * Fist loop: iterates over the array
+ * Second loop: iterates over the properties of the array
+ * 
+ */
+<html>
+<head>
+</head>
+<body>
+
+
+
+<script>
+
+window.onload = function() {
+
+// create a couple of elements in an otherwise empty HTML page
+const heading = document.createElement("h1");
+const heading_text = document.createTextNode("Big Head!");
+heading.appendChild(heading_text);
+document.body.appendChild(heading);
+}
+
+
+
+
+
+{/* let array = [
+    {
+        x: 0,
+        y: 0,
+        z: 0
+    },
+    {
+        a:0,
+        b:0,
+        c:0
+    }
+]
+
+for (let i =0; i < 5; i++) {
+        array.map((prop) => {
+            {this.prop = i}
+        })[0]
+    for (j = 1; j < 10; j++) {    
+        array[i][j]
+    }
+}
+console.log(array) */}
+
+
+</script>
+
+</body>
+</html>
+
